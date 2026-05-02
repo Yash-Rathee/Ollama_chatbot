42702d58d1d1e267a224cd90017f9262b2803fe7
Yash Rathee <69687093+Yash-Rathee@users.noreply.github.com>
2026-05-03 00:20:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,17 +43,20 @@ app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 app.run_polling()
 
 import threading
+import os
 from flask import Flask
 
 app_flask = Flask(__name__)
 
 @app_flask.route('/')
 def home():
-    return "Bot is running"
+    return "Hades is alive 🔥"
 
 def run_bot():
     app.run_polling()
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()
-    app_flask.run(host="0.0.0.0", port=10000)
+    
+    port = int(os.environ.get("PORT", 10000))  # IMPORTANT
+    app_flask.run(host="0.0.0.0", port=port)
